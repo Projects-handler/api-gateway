53af9bc3a2f02454996a4028b11187a54ce922c3
leonardo <quevedoruiz41@gmail.com>
2024-12-22 16:58:44 -0500

    Add global vars and add projects handler

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './common/envs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = envs.port ?? 3000;
+  const HOST = envs.host;
+  const logger = new Logger('api-gateway');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
+  logger.log(`Server running on http://${HOST}:${PORT}`);
 }
 bootstrap();
